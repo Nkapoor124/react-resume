804e9e5200f44ec5dc30f001edce8bd5c57bcf15
abahr35 <austinbahr97@gmail.com>
2024-10-18 14:03:49 -0500

    Contact Me - updated

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -30,10 +30,10 @@ const ContactForm: FC = memo(() => {
   );
 
   const handleSendMessage = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
+  async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-      try {
+    try {
       const response = await fetch('/api/contact', {
         method: 'POST',
         headers: {
@@ -58,7 +58,8 @@ const ContactForm: FC = memo(() => {
     }
   },
   [data, defaultData],
-  );
+);
+
 
   const inputClasses =
     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
