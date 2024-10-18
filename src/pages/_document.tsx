b16045f3f87dca7507f3da43449075ea79772243
abahr35 <austinbahr97@gmail.com>
2024-10-18 12:40:29 -0500

    Changed Page.tsx to actually make the Imessage share work

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,19 @@
 import {Head, Html, Main, NextScript} from 'next/document';
 
+// next/document <Head /> vs next/head <Head />
+//
+// next/document Head is rendered once on the server. This is different from next/head which will
+// rebuild the next/head fields each time it's called, and won't overwrite next/document's Head.
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        {/* Prevent Google Translate */}
+        {/* google translate breaks react:
+          - https://github.com/facebook/react/issues/11538
+          - https://bugs.chromium.org/p/chromium/issues/detail?id=872770 */}
         <meta content="notranslate" name="google" />
-
-        {/* Open Graph Meta Tags */}
-        <meta content="Nevin Kapoor Resume" property="og:title" />
-        <meta content="A resume website for Nevin Kapoor to showcase his skills and certifications" property="og:description" />
-        <meta content="https://www.nevinkapoor.com" property="og:url" />
-        <meta content="website" property="og:type" />
-        <meta content="Nevin Kapoor Resume" property="og:site_name" />
-
-        {/* Twitter Cards */}
-        <meta content="Nevin Kapoor Resume" name="twitter:title" />
-        <meta content="A resume website for Nevin Kapoor to showcase his skills and certifications" name="twitter:description" />
-
       </Head>
       <body className="bg-black">
         <Main />
